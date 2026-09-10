3587c7cab5cfc25585c5536aa6aa67462629b59a
chinmck <chinmck@gmail.com>
2026-09-10 15:10:43 +0100

    Add Google Ads conversion and campaign tracking

--- public/script.js
+++ public/script.js
@@ -2,8 +2,43 @@
   'use strict';
 
   var META_PIXEL_ID = '1377605307888175';
-  var ATTRIBUTION_KEYS = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_content', 'utm_term', 'fbclid'];
+  var GOOGLE_ADS_ID = 'AW-18409587176';
+  var GOOGLE_ADS_CONVERSIONS = {
+    quoteRequest: 'q426CJiPjPMcEOiDsMpE',
+    telephoneClick: 'YZE1CJuPjPMcEOiDsMpE',
+    whatsappClick: 'cy1XCJ6PjPMcEOiDsMpE'
+  };
+  var ATTRIBUTION_KEYS = [
+    'utm_source', 'utm_medium', 'utm_campaign', 'utm_content', 'utm_term',
+    'gclid', 'gbraid', 'wbraid', 'fbclid', 'partner', 'partner_id'
+  ];
   var STORAGE_KEY = 'jettset_campaign_attribution';
+  var googleTrackedForms = typeof WeakSet === 'function' ? new WeakSet() : null;
+
+  function installGoogleAdsTag() {
+    if (window.__jettsetGoogleAdsInstalled) return;
+    window.__jettsetGoogleAdsInstalled = true;
+    window.dataLayer = window.dataLayer || [];
+    window.gtag = window.gtag || function () {
+      window.dataLayer.push(arguments);
+    };
+
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=' + encodeURIComponent(GOOGLE_ADS_ID);
+    document.head.appendChild(script);
+
+    window.gtag('js', new Date());
+    window.gtag('config', GOOGLE_ADS_ID);
+  }
+
+  function googleAdsConversion(label, parameters) {
+    if (typeof window.gtag !== 'function' || !label) return;
+    var eventData = parameters || {};
+    eventData.send_to = GOOGLE_ADS_ID + '/' + label;
+    eventData.event_timeout = 2000;
+    window.gtag('event', 'conversion', eventData);
+  }
 
   function readCookie(name) {
     var prefix = name + '=';
@@ -31,6 +66,13 @@
       var value = params.get(key);
       if (value) attribution[key] = value;
     });
+
+    if (!attribution.landing_page) {
+      attribution.landing_page = window.location.pathname + window.location.search;
+    }
+    if (!attribution.referrer && document.referrer) {
+      attribution.referrer = document.referrer;
+    }
 
     var fbc = readCookie('_fbc');
     var fbp = readCookie('_fbp');
@@ -101,10 +143,19 @@
         content_name: formName || 'jettset-enquiry',
         enquiry_type: formName || 'jettset-enquiry'
       });
+
+      if (!form || !googleTrackedForms || !googleTrackedForms.has(form)) {
+        if (form && googleTrackedForms) googleTrackedForms.add(form);
+        googleAdsConversion(GOOGLE_ADS_CONVERSIONS.quoteRequest, {
+          form_name: formName || 'jettset-enquiry',
+          page_path: window.location.pathname
+        });
+      }
     }
   };
 
   function initialiseTracking() {
+    installGoogleAdsTag();
     collectAttribution();
     document.querySelectorAll('form[name^="jettset-"]').forEach(enrichForm);
 
@@ -122,8 +173,10 @@
 
       if (/^tel:/i.test(href)) {
         metaEvent('trackCustom', 'PhoneClick', eventData);
+        googleAdsConversion(GOOGLE_ADS_CONVERSIONS.telephoneClick, eventData);
       } else if (/^(?:https?:)?\/\/(?:wa\.me|(?:api\.)?whatsapp\.com)\//i.test(href)) {
         metaEvent('trackCustom', 'WhatsAppClick', eventData);
+        googleAdsConversion(GOOGLE_ADS_CONVERSIONS.whatsappClick, eventData);
       }
     });
   }
